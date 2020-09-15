d4ffb2f3ff864745b986ccfd9dc2cb33657be93e
juholehto <69836633+juholehto@users.noreply.github.com>
2020-09-15 11:59:48 +0300

    15.09.2020

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { VictoryChart, VictoryLine, VictoryBar } from 'victory'
+import React, { useState } from 'react'
+import { VictoryChart, VictoryLine, VictoryBar, VictoryVoronoiContainer } from 'victory'
 
 function Weather() {
 
@@ -10,8 +10,39 @@ function Weather() {
         { paivamaara: "4.1", ilmankosteus: 70, label: '70 %' }
     ];
 
+    const today = new Date();
+    const date = today.getDate() + "." + parseInt(today.getMonth() + 1) + "." + today.getFullYear();
+
+    const initWeather = [];
+    const [weather, setWeather] = useState(initWeather);
+
+
+
+    fetch('https://funcvariaiot.azurewebsites.net/api/HttpTriggerGetIotData?code=qO5qkShg0osHqY0BB2nfXI/anPgQ/K/3mIF7VTCFfaTdrvo6wl6DKw==')
+        .then(response => response.json())
+        .then(json => setWeather([...json]));
+
+    const rows = () => weather.slice(0, 24).reverse().map(temphum => {
+        const measurementDate = temphum.PublishedAt.split('T')[0].split('-')[2] + '.' + temphum.PublishedAt.split('T')[0].split('-')[1] + temphum.PublishedAt.split('T')[0].split('-')[0]
+        const measurementTime = temphum.PublishedAt.split ('T')[1].split(':')[0] + ':' +  temphum.PublishedAt.split ('T')[1].split(':')[0]
+
+        return <div><b>Pvm: </b>{measurementDate}, <b>klo:</b> {measurementTime} -------- <b>Ilmankosteus: </b> {temphum.Hum.split('.')[0]}% -------- <b>Lämpötila:</b> {temphum.Temp.split('.')[0]}°C</div>
+    })
+
     return (
         <div align="center">
+            <div>
+                <h3>Piirrettävän chartin data</h3>
+            </div>
+            <div>
+                <b>Tänään on : {date} </b>
+            </div>
+            <div>
+                {rows()}
+            </div>
+            <div>
+                Sensoridata
+            </div>
 
             <h3>Lämpötila</h3>
 
